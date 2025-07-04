818ab35cead87c9f9da3472f616b7da5fe1db35e
ckrashh <carrillo.gerald.a.s@gmail.com>
2025-07-03 23:00:05 -0400

    ahora si que si

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,7 @@
 function optenerDatoBoton(id) {
     // Obtener el valor del botón
-    var boton = id.value;
+    var boton = $(id).val(); // Obtener el valor del botón
     // Mostrar el valor en la consola
     return boton;
 }
 
-function ocultarBoton(id) {
-    // Ocultar el botón
-    $(id).parent().find("p").remove(); // Elimina el párrafo
-    $(id).remove(); // Elimina el botón "Ver menos"
-    $(".botonVer").toggle(); // Muestra el botón "Ver más" nuevamente
-}
