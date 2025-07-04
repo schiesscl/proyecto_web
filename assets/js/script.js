ee8c1c04d0416b8543936adf973a957d6fead186
ckrashh <carrillo.gerald.a.s@gmail.com>
2025-07-03 22:34:05 -0400

    arreglar funciones

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,10 @@ function optenerDatoBoton(id) {
     // Mostrar el valor en la consola
     return boton;
 }
+
+function ocultarBoton(id) {
+    // Ocultar el botón
+    $(id).parent().find("p").remove(); // Elimina el párrafo
+    $(id).remove(); // Elimina el botón "Ver menos"
+    $(".botonVer").toggle(); // Muestra el botón "Ver más" nuevamente
+}
