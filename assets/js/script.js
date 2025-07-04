fc69a4c97a447490b720c9b3702fbc53d37c8abc
ckrashh <carrillo.gerald.a.s@gmail.com>
2025-07-03 23:58:19 -0400

    esta si que si

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 function optenerDatoBoton(id) {
     // Obtener el valor del botón
-    var boton = $(id).val(); // Obtener el valor del botón
+    var boton = $(id).attr("value"); // Obtener el valor del botón
     // Mostrar el valor en la consola
     return boton;
 }
